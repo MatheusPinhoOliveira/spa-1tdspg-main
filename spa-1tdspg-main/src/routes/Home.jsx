30de2692b8d74209caa3632c93d29c5c16ccd61c
MatheusPinhoOliveira <matheuspinho736@gmail.com>
2023-10-04 08:58:50 -0300

    first commit

---
+++ spa-1tdspg-main/src/routes/Home.jsx
@@ -0,0 +1,16 @@
+export default function Home(){
+document.title = "Home"
+    return(
+        <>
+            <div>
+                <h1>Produtos em Oferta.....</h1>
+                <figure>
+                    <figcaption>
+
+                    </figcaption>
+                    <img src="/online-shopping_640.png" alt="" />
+                </figure>
+            </div>
+        </>
+    )
+}
